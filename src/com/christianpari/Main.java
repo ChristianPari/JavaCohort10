a011ffac92f74b954c1db5766ce205245a8ca7be
Christian Pari <ChristianPari@outlook.com>
2020-10-19 21:10:39 -0400

    Understanding Java basics

---
+++ src/com/christianpari/Main.java
@@ -0,0 +1,9 @@
+package com.christianpari;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CareerDevs");
+        var myCar = new Car("Blue", "Subaru", "1234", 2021);
+    }
+}
